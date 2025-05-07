65b162f2af613171a833f692028a069a2adbd8c0
Tarun <tarunchauhan0908@gmail.com>
2025-05-07 14:12:14 +0530

    updating2

--- netlify/functions/init-repo.js
+++ netlify/functions/init-repo.js
@@ -103,7 +103,6 @@ Start editing your documentation here.
       CMS.init({
         config: {
           backend: {
-            load_config_file: false,
             name:   'git-gateway',
             repo:   repo,      // ← tell CMS which repo to use
             branch: 'main'
